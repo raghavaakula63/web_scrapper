de56154a49166d28898cd1fa1087a04b450406cf
Raghavendra Aakula <raghavaakula63@gmail.com>
2024-06-11 14:01:16 -0400

    first commit

--- app.py
+++ app.py
@@ -3,7 +3,10 @@ from flask_cors import CORS,cross_origin
 import requests
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen as uReq
+import urllib
+from pymongo.mongo_client import MongoClient
 import logging
+
 logging.basicConfig(filename="scrapper.log" , level=logging.INFO)
 
 app = Flask(__name__)
@@ -22,15 +25,15 @@ def index():
             flipkartPage = uClient.read()
             uClient.close()
             flipkart_html = bs(flipkartPage, "html.parser")
-            bigboxes = flipkart_html.findAll("div", {"class": "_1AtVbE col-12-12"})
+            bigboxes = flipkart_html.findAll("div", {"class": "cPHDOP col-12-12"})
             del bigboxes[0:3]
             box = bigboxes[0]
             productLink = "https://www.flipkart.com" + box.div.div.div.a['href']
-            prodRes = requests.get(productLink)
+            prodRes = uReq(productLink)
             prodRes.encoding='utf-8'
-            prod_html = bs(prodRes.text, "html.parser")
+            prod_html = bs(prodRes, "html.parser")
             print(prod_html)
-            commentboxes = prod_html.find_all('div', {'class': "_16PBlm"})
+            commentboxes = prod_html.find_all('div', {'class': "RcXBOT"})
 
             filename = searchString + ".csv"
             fw = open(filename, "w")
@@ -40,7 +43,7 @@ def index():
             for commentbox in commentboxes:
                 try:
                     #name.encode(encoding='utf-8')
-                    name = commentbox.div.div.find_all('p', {'class': '_2sc7ZR _2V5EHH'})[0].text
+                    name = commentbox.div.div.find_all('p', {'class': '_2NsDsF AwS1CA'})[0].text
 
                 except:
                     logging.info("name")
@@ -71,7 +74,23 @@ def index():
                 mydict = {"Product": searchString, "Name": name, "Rating": rating, "CommentHead": commentHead,
                           "Comment": custComment}
                 reviews.append(mydict)
+
             logging.info("log my final result {}".format(reviews))
+            
+            try:
+                uri = "mongodb+srv://admin:"+urllib.parse.quote('9912439639Aa@')+"@cluster0.at8clbw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+                client = MongoClient(uri)
+                db=client['web_scrapper']
+                collection=db['flipkart_scrapper']
+                collection.insert_many(reviews)
+                logging.info('sucessfully data is inserter')
+            except Exception as e:
+                logging.info('error is {}'.format(e))
+
+            
+            
+            
             return render_template('result.html', reviews=reviews[0:(len(reviews)-1)])
         except Exception as e:
             logging.info(e)
